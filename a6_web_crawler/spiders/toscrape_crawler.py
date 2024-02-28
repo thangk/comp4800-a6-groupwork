f0d26462fd6574f612e28007d2523ed05c5d151c
thangk <thangk@uwindsor.ca>
2024-02-28 12:49:45 -0500

    Kap: update

--- a6_web_crawler/spiders/toscrape_crawler.py
+++ a6_web_crawler/spiders/toscrape_crawler.py
@@ -4,8 +4,8 @@ from scrapy.exceptions import CloseSpider
 
 class ToscrapeCrawlerSpider(Spider):
     name = "toscrape-crawler"
-    allowed_domains = ["toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
+    allowed_domains = ["kapthang.dev"]
+    start_urls = ["https://kapthang.dev"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,13 +14,14 @@ class ToscrapeCrawlerSpider(Spider):
         self.nodes = set()
         self.nodes_with_children = set()  # Track nodes that have outgoing edges
         self.nodes_without_children = set()  # Track nodes without outgoing edges (leaf nodes)
-        self.nodes_limit = 10  # Set to 100 for testing, or -1 for no limit
+        self.nodes_limit = -1  # Set to 100 for testing, or -1 for no limit
 
     def parse(self, response):
         if response.url not in self.url_to_id:
             self.url_to_id[response.url] = self.id_counter
             self.nodes.add(self.id_counter)
             self.id_counter += 1
+            self.seen_edges = set()  # Add this line to initialize the set of seen edges
 
         source_id = self.url_to_id[response.url]
         has_children = False  # Assume no children until found
@@ -33,9 +34,11 @@ class ToscrapeCrawlerSpider(Spider):
                 self.id_counter += 1
 
             target_id = self.url_to_id[target_url]
-            yield LinkGraphItem(source=source_id, target=target_id)
-            has_children = True  # Found a child node
-            self.nodes_with_children.add(source_id)  # Mark source node as having outgoing edges
+            if (source_id, target_id) not in self.seen_edges:
+                self.seen_edges.add((source_id, target_id))  # Mark this edge as seen
+                yield LinkGraphItem(source=source_id, target=target_id)
+                has_children = True  # Found a child node
+                self.nodes_with_children.add(source_id)  # Mark source node as having outgoing edges
 
             if self.nodes_limit == -1 or len(self.nodes) <= self.nodes_limit:
                 yield Request(target_url, callback=self.parse)
